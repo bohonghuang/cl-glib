992f55e31fd4f44b8371b3465a3acb7db72ed7d9
coco24 <1281299809@qq.com>
2022-09-18 19:33:57 +0800

    Add an empty `:use` clause to `defpackage`

--- package.lisp
+++ package.lisp
@@ -15,7 +15,9 @@
 ;;;; You should have received a copy of the GNU Lesser General Public License
 ;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(cl:defpackage gio (:export #:*ns*))
+(cl:defpackage gio
+  (:use)
+  (:export #:*ns*))
 
 (in-package #:gio)
 
